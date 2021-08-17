b4de85443770b3b0b81e37cbc02edd72caed02de
byCrookie <maeni.h01@hotmail.com>
2021-08-17 22:19:43 +0200

    Public Log4NetConfigurator

--- Framework.NET/Framework/Autofac/Logger/Log4NetConfigurator.cs
+++ Framework.NET/Framework/Autofac/Logger/Log4NetConfigurator.cs
@@ -6,8 +6,15 @@ using log4net.Config;
 
 namespace Framework.Autofac.Logger
 {
-    internal static class Log4NetConfigurator
+    public static class Log4NetConfigurator
     {
+        public static void Configure(ContainerBuilder containerBuilder)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var module = new Log4NetMiddlewareModule(new Log4NetMiddleware());
+            containerBuilder.RegisterModule(module);
+        }
+        
         public static void Configure(ContainerBuilder containerBuilder, string log4NetConfigurationFile)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
