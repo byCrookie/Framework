fc716b01dc1f103f0ee5b0fb3b44e75febf82989
byCrookie <maeni.h01@hotmail.com>
2022-07-15 19:34:32 +0200

    implicit dependencies

--- tests/Framework.Boot.Tests/WorkflowServiceProvider.cs
+++ tests/Framework.Boot.Tests/WorkflowServiceProvider.cs
@@ -9,6 +9,6 @@ namespace Framework.Tests;
 [Transient(typeof(IWorkflowBuilder<WorkflowTestContext>), typeof(WorkflowBuilder<WorkflowTestContext>))]
 [Transient(typeof(IWorkflowTestStep<WorkflowTestContext>), typeof(WorkflowTestStep<WorkflowTestContext>))]
 [Transient(typeof(IWorkflowTestOptionsStep<WorkflowTestContext, WorkflowTestStepOptions>), typeof(WorkflowTestOptionsStep<WorkflowTestContext, WorkflowTestStepOptions>))]
-internal partial class WorkflowServiceProvider
+public partial class WorkflowServiceProvider
 {
 }
