6f894c4338f2970b849e9eade021d2ae746b7cd4
byCrookie <maeni.h01@hotmail.com>
2022-07-15 20:12:30 +0200

    removed registration

--- src/Framework.Boot/FrameworkBootModule.cs
+++ src/Framework.Boot/FrameworkBootModule.cs
@@ -1,6 +1,5 @@
 ﻿using DependencyInjection.Microsoft.Modules;
 using Microsoft.Extensions.DependencyInjection;
-using Workflow;
 using Workflow.DependencyInjection;
 
 namespace Framework.Boot;
@@ -9,8 +8,6 @@ public class FrameworkBootModule : Module
 {
     public override void Load(IServiceCollection services)
     {
-        services.AddTransient(typeof(IWorkflowBuilder<BootContext>), typeof(WorkflowBuilder<BootContext>));
-        
         AddModule(new BootModule());
         AddModule(new WorkflowModule());
         
